2fa884e70bd7f4996bdd27821fb6f90926091d78
Devasurya-S <devasuryasuresh@gmail.com>
2024-01-04 16:15:50 +0530

    Initialized authentication using google but errors needs to fixed, another runtime error related to laytout.jsx poped up

--- promptopia/components/Nav.jsx
+++ promptopia/components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
-    const isUserLogged = true;
+    const {data:session} = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
 
     useEffect(() => { 
-      const setProviders = async () => {
+      const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
       }
-      setProviders();
+      setUpProviders();
     }, [])
 
   return (
@@ -37,7 +37,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLogged ? (
+        {session?.user ? (
             <div className='flex gap-3 md:gap-5'>
                 <Link href="/create-prompt" className="black_btn">
                     Create Post
@@ -77,7 +77,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLogged ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
               src="/assets/images/logo.svg"
