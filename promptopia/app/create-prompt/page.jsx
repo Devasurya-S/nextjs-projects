d6f69cd2813287e2dabd2d6577f6efa5134c81d5
Devasurya-S <devasuryasuresh@gmail.com>
2024-01-08 16:06:45 +0530

    fetch function for create post started loging off for today

--- promptopia/app/create-prompt/page.jsx
+++ promptopia/app/create-prompt/page.jsx
@@ -14,7 +14,14 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
+        e.preventDefault(); //prevents the default action of browser to perform a reload
+        setSubmitting(true);
 
+        try {
+            const response = await fetch() //last here
+        } catch (error) {
+
+        }
     }
     return (
         <Form
